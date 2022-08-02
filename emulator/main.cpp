61ecfa828bcde71b241e7d5f07e4e4c37e999de1
Arto Chydenius <arto.chydenius@reaktor.com>
2022-08-02 13:11:48 +0300

    Add initial SDL window

---
+++ emulator/main.cpp
@@ -0,0 +1,31 @@
+#include <SDL2/SDL.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+int main(int argc, char **argv) {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window = SDL_CreateWindow("Voltage Oscilloscope Emulator", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  bool quit = false;
+  SDL_Event event;
+  while (!quit) {
+    while (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT ||
+          (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
+        quit = 1;
+      }
+    }
+
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
